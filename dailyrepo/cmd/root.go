1451151d8372bec7f83dd52e46659c10cec70ed2
tjmtmmnk <tsujimoto027@gmail.com>
2019-06-25 18:06:33 +0900

    cobra追加

---
+++ dailyrepo/cmd/root.go
@@ -0,0 +1,57 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"github.com/spf13/cobra"
+
+	homedir "github.com/mitchellh/go-homedir"
+	"github.com/spf13/viper"
+)
+
+var cfgFile string
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "dailyrepo",
+	Short: "A brief description of your application",
+	Long: `A longer description that spans multiple lines and likely contains
+examples and usage of using your application. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+}
+
+func initConfig() {
+	if cfgFile != "" {
+
+		viper.SetConfigFile(cfgFile)
+	} else {
+
+		home, err := homedir.Dir()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		viper.AddConfigPath(home)
+		viper.SetConfigName(".dailyrepo")
+	}
+
+	viper.AutomaticEnv() // read in environment variables that match
+
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	}
+}
---
+++ dailyrepo/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/tjmtmmnk/go-cli-hands-on/cmd/dailyrepo/cmd"
+
+func main() {
+  cmd.Execute()
+}
